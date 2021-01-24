693d2308c8f9992333fe0f1f0ef568892bd13055
Austin Meyer <austinmeyer@austins-mbp.lan>
2021-01-24 12:47:26 -0600

    Terraform part 1 complete

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,23 @@
-# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  access_key = "AccessKey"
+  secret_key = "SecretKey"
+  region = "us-west-2"
+}
 
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-0a36eb8fadc976275"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
-
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "ami-0a36eb8fadc976275"
+  instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+  }
+}
